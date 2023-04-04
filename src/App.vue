d86a956e864e558ec8d23796bdc5a24dedd508f3
dev-edsonlopes <dev_edson@outlook.com>
2023-04-04 12:24:42 -0300

    SCREEN LOGIN CREATED COMMIT

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-    <FormLogin />
-
+  <div>
+    <Login :logo="logo_src" :alt="alt_name"/>
+    <router-view />
+  </div>
 </template>
 <script>
-import FormLogin from "./components/FormLogin.vue";
-
+import Login from "./components/Login.vue";
 export default {
-  name:"App",
+  name: "App",
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "mobs2",
+    }
+  },
   components: {
-    FormLogin
-}
-}
+    Login,
+  },
+};
 </script>
+<style>
+* {
+  font-family: Helvetica, sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,112 @@
+<template>
+  <div id="main-container">
+    <div id="container-left"></div>
+    <div id="container-right">
+      <form>
+        <router-link to="/" id="logo-url">
+          <img :src="logo" :alt="alt" id="logo" />
+        </router-link>
+        <div class="login-senha">
+          <input type="email" v-model="email" placeholder="USUÁRIO" />
+        </div>
+        <div class="login-senha">
+          <input type="password" v-model="password" placeholder="SENHA" />
+        </div>
+        <div class="btn">
+          <input type="submit" @click="login" value="Entrar" />
+        </div>
+        <div class="btn">
+          <input type="submit" @click="login" value="Criar conta" />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Login",
+  props: ["logo", "alt"],
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async login() {
+        
+    },
+    createAccount() {
+
+    },
+  },
+};
+</script>
+<style scoped>
+#main-container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+}
+#container-left {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  background-image: url("bg_login.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+#container-right {
+  height: 100%;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+form {
+  width: 450px;
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+}
+
+form #logo {
+  max-width: 400px;
+  margin-bottom: 20px;
+  margin-left: 25px;
+}
+
+form input {
+  border: none;
+  width: 100%;
+  box-sizing: border-box;
+  height: 35px;
+  font-size: 1rem;
+}
+form input:focus {
+  box-shadow: 0 0 0 0;
+  outline: 0;
+}
+form .login-senha {
+  margin: 10px 0px;
+  border-bottom: 3px solid #50bf82;
+  border-radius: 5px;
+}
+
+form > .btn {
+  margin: 5px auto;
+}
+form > .btn > input {
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: #50bf82;
+  color: #fff;
+  height: 30px;
+  width: 150px;
+  cursor: pointer;
+}
+</style>
